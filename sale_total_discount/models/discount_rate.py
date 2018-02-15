fce5906d3239480d7a91fd107d6624451649969e
Manuel Marquez <buzondemam@gmail.com>
2018-02-14 22:45:40 -0400

    [ADD] sale_total_discount: adds models discount.rate and discount.rate.line

---
+++ sale_total_discount/models/discount_rate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Uakami - Manuel Marquez <buzondemam@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class DiscountRate(models.Model):
+    _name = 'discount.rate'
+
+    name = fields.Char(required=True)
+    line_ids = fields.One2many(
+        'discount.rate.line', 'discount_rate_id', 'Items')
+
+
+class DiscountRateLine(models.Model):
+    _name = 'discount.rate.line'
+
+    min_qty = fields.Float('Minimum Quantity', required=True)
+    discount_type = fields.Selection([
+        ('percentage', 'Percentage'), ('fixed', 'Amount Fixed')
+    ], 'Discount Type', required=True)
+    percentage = fields.Float('Percentage')
+    amount = fields.Float('Amount')
+    discount_rate_id = fields.Many2one('discount.rate', 'Discount Rate')
