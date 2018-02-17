898af3a7e1c2c5e0e7d6f958a6589ff1687d8b15
Manuel Marquez <buzondemam@gmail.com>
2018-02-17 11:35:09 -0400

    [ADD] sale_show_total_discount: show total discount in sales orders

---
+++ sale_show_total_discount/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Uakami - Manuel Marquez <buzondemam@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Show Total Discount in Sales Orders',
+    'version': '11.0.0.1.0',
+    'category': 'Sale',
+    'author': 'Uakami',
+    'website': "https://uakami.com/",
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
